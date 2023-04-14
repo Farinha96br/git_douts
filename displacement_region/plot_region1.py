ce0847c997b3412456a9fffbe801442bca8dcb36
Farinha <farinha96br@gmail.com>
2023-04-14 18:27:15 +0200

    plot arrumado

--- displacement_region/plot_region1.py
+++ displacement_region/plot_region1.py
@@ -16,8 +16,8 @@ cym_light =  ['#82e7ff','#fde974','#ff98ff']
 cym_pallet = ['#00ceff','#ffd700','#ff6dff']
 cym_pallet = ['#007a96','#b39700','#b04bb0']
 
-cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white",rgb_pallet[2]])
-cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list("", [rgb_pallet[2],"black",rgb_pallet[0]])
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white",rgb_darker[2]])
+cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list("", [rgb_pallet[2],"white",rgb_pallet[0]])
 
 folder = sys.argv[1]
 
@@ -28,12 +28,15 @@ fig.set_size_inches(18*0.393, 14*0.393) # diminuir na metade p
 ax.set_ylabel("$x$")
 ax.set_xlabel("$y$")
 print(data.shape)
-L = 100
+L = 500
 x = np.reshape(data[:,0],(L,L))
 y = np.reshape(data[:,1],(L,L))
 z = np.reshape(data[:,2],(L,L))
 
+d = np.abs(z-x)
 
 
-ax.pcolormesh(y, x,z,cmap=cmap)
+
+ax1 = ax.pcolormesh(y, x,d,cmap=cmap)
+fig.colorbar(ax1,label = r"|\Delta x|")
 plt.savefig(folder + "/region.png", bbox_inches='tight', dpi = 300)
